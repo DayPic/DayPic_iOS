7ae4ae22517a76a714531b2c65f1a30e79f31d24
feldblume5263 <feldblume5263@gmail.com>
2024-05-22 01:30:57 +0900

    Add basic functionality to PhotoOrganizerSampleApp viewModel
    - Implemented methods in viewModel to load Encoders
    - Added method to check category similarity
    - Enabled basic operations using PhotoOrganizer module in SampleApp

--- PhotoOrganizerSampleApp/PhotoOrganizerSampleApp/SampleView/SampleViewController.swift
+++ PhotoOrganizerSampleApp/PhotoOrganizerSampleApp/SampleView/SampleViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import DownpourRx
+import SnapKit
 
 final class SampleViewController: UIViewController {
     
-    let viewModel: SampleViewModel
+    private let viewModel: SampleViewModel
+    
+    private let disposeBag: DisposeBag = .init()
+    
+    private let testButton: UIButton = {
+        let button = UIButton()
+        button.layer.cornerRadius = 10.0
+        button.setTitle("start test", for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setTestButtonLayout()
+        bind()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.viewModel.loadPhotoOrganizer()
+    }
+    
+    private func bind() {
+        viewModel.isLoadingRelay
+            .withUnretained(self)
+            .observe(on: MainScheduler.instance)
+            .bind { owner, isLoading in
+                owner.testButton.backgroundColor = isLoading ? .lightGray : .magenta
+                owner.testButton.isEnabled = !isLoading
+            }
+            .disposed(by: disposeBag)
     }
     
     init(viewModel: SampleViewModel) {
@@ -25,6 +52,14 @@ final class SampleViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private func setTestButtonLayout() {
+        self.view.addSubview(self.testButton)
+        self.testButton.snp.makeConstraints { make in
+            make.width.equalTo(100)
+            make.height.equalTo(30)
+            make.center.equalToSuperview()
+        }
+    }
 
 }
 
--- PhotoOrganizerSampleApp/PhotoOrganizerSampleApp/SampleView/SampleViewModel.swift
+++ PhotoOrganizerSampleApp/PhotoOrganizerSampleApp/SampleView/SampleViewModel.swift
@@ -6,11 +6,54 @@
 //
 
 import PhotoOrganizer
+import DownpourRx
+import UIKit
 
 final class SampleViewModel {
     private let photoOrganizer: PhotoOrganizer
+    private let categories: [String]
     
-    init(photoOrganizer: PhotoOrganizer) {
+    let isLoadingRelay: BehaviorRelay<Bool> = .init(value: false)
+    
+    init(
+        photoOrganizer: PhotoOrganizer
+    ) {
         self.photoOrganizer = photoOrganizer
+        self.categories = ["food", "scene", "human", "animal", "nature", "city", "product", "exercise"]
     }
+    
+    func loadPhotoOrganizer() {
+        print("Start Loading Encoders ...")
+        
+        Task {
+            let start = DispatchTime.now()
+            
+            self.isLoadingRelay.accept(true)
+            await self.photoOrganizer.loadEncoders()
+            self.isLoadingRelay.accept(false)
+            
+            let end = DispatchTime.now()
+            let nanoTime = end.uptimeNanoseconds - start.uptimeNanoseconds
+            let timeInterval = Double(nanoTime) / 1_000_000_000
+            print("loadPhotoOrganizer took \(timeInterval) seconds.")
+        }
+        
+    }
+    
+    func getCategorySimilarity(image: UIImage) async -> [String: Float] {
+        let start = DispatchTime.now()
+        
+        let res = await self.photoOrganizer.computeSimilarity(
+            image: image,
+            categories: categories
+        )
+        
+        let end = DispatchTime.now()
+        let nanoTime = end.uptimeNanoseconds - start.uptimeNanoseconds
+        let timeInterval = Double(nanoTime) / 1_000_000_000
+        print("getSimilarityWith took \(timeInterval) seconds. \n \(res) \n\n")
+        
+        return res
+    }
+    
 }
